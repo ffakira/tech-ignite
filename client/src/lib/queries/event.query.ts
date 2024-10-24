ef44fa3dc514f2b39d2ab00f0c40895ca6753deb
ffakira <55924290+ffakira@users.noreply.github.com>
2024-10-23 22:02:23 -0300

    impl: event queries

--- client/src/lib/queries/event.query.ts
+++ client/src/lib/queries/event.query.ts
@@ -1,3 +1,61 @@
-/**
- * @TODO implement queries for events
- */
+import { QueryOptions, queryOptions } from "@tanstack/react-query";
+
+export function useEventQuery(id: number, options?: QueryOptions) {
+  return queryOptions({
+    queryKey: ["event", id],
+    queryFn: async () => {
+      try {
+        const response = await fetch(
+          `${import.meta.env.VITE_API_V1_URL}/events/${id}`,
+          {
+            method: "GET",
+            headers: {
+              Accept: "application/json",
+            },
+          }
+        );
+
+        if (!response.ok) {
+          return { status: "error", errors: ["An error occurred"] } as const;
+        }
+
+        const data = await response.json();
+        return { status: "success", data } as const;
+      } catch (error) {
+        console.error(error);
+        return { status: "error", errors: ["Internal server error"] } as const;
+      }
+    },
+    ...options,
+  });
+}
+
+export function useEventsQuery(options?: QueryOptions) {
+  return queryOptions({
+    queryKey: ["events"],
+    queryFn: async () => {
+      try {
+        const response = await fetch(
+          `${import.meta.env.VITE_API_V1_URL}/events`,
+          {
+            method: "GET",
+            headers: {
+              Accept: "application/json",
+            },
+          }
+        );
+
+        if (!response.ok) {
+          return { status: "error", errors: ["An error occurred"] } as const;
+        }
+
+        const data = await response.json();
+        return { status: "success", data } as const;
+      } catch (error) {
+        console.error(error);
+        return { status: "error", errors: ["Internal server error"] } as const;
+      }
+    },
+    ...options,
+  });
+}
