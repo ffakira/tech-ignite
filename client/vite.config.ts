cddd6d1c9d00f71fef87ded1cf288fb445fe95e9
ffakira <55924290+ffakira@users.noreply.github.com>
2024-10-25 16:11:05 -0300

    chore: added Dockerfile for client

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,4 +5,20 @@ import tsConfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsConfigPaths()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+  },
+  preview: {
+    port: 5173,
+    strictPort: true,
+  },
+  server: {
+    proxy: {
+      "/api/v1": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
